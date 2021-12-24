875a7ac1fbbe57d97c57734d36c6914c93e7e99d
Lucas Guzman-Finn <lggufi@gmail.com>
2021-12-24 01:24:25 -0500

    full

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Login from './components/Login';
 
 function App() {
+  const hash = window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce(function (initial, item) {
+      if (item) {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
+
+  const generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  const [token] = useState(hash.access_token)
+  const [state, setState] = useState(localStorage.getItem("state"))
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    if (!state) {
+      let _state = generateRandomString(16)
+      setState(_state)
+      localStorage.setItem('state', _state)
+    }
+  }, [state])
+
+  const getItems = () => {
+    console.log("fetching items")
+    const data = { limit: 10, time_range: "long_term" }
+    const url = `https://api.spotify.com/v1/me/top/tracks?time_range=${encodeURIComponent(data.time_range)}&limit=${encodeURIComponent(data.limit)}`
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + token
+      },
+    }).then(res => {
+      if (res.status === 200) {
+        res.json().then(res => setItems(res.items))
+      } else if (res.status === 429) {
+        const retry_after = res.headers['Retry-After'] + 1
+        setTimeout('', retry_after * 1000);
+        getItems();
+        console.log("RATE LIMITED", res.status, retry_after)
+      } else if (res.status === 401) {
+        console.log("ERROR AUTH", res.status)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (token) {
+      getItems()
+    }
+  }, [token])
+
+  useEffect(() => {
+    console.log("items:", items)
+  }, [items])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token && (
+        <Login state={state}/>
+      )}
+      {items && (
+        items.map((item, i) => (
+          <h1 key={i}>{item.name}</h1>
+        )
+        ))}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,19 @@
+function Login(props) {
+    const authEndpoint = "https://accounts.spotify.com/authorize";
+    const clientId = "3bebcbc76113406eadcaeb5051d437ad";
+    const redirectUri = "http://localhost:3000";
+    const scopes = [
+      "user-read-private",
+      "user-read-email",
+      "user-top-read",
+    ];
+    const state = props.state
+
+    return (
+        <a href={`${authEndpoint}?response_type=token&client_id=${clientId}&scope=${scopes.join("%20")}&redirect_uri=${redirectUri}&state=${state}`}>
+            Login
+        </a>
+    )
+}
+
+export default Login;
